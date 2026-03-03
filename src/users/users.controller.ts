3fe461798791fc33636253a3db68616e2f58b5d5
unknown <thanhdaicoder@gmail.com>
2026-03-03 20:02:18 +0700

    Revert " delete testing file"
    This reverts commit ccaa281295ab091186de61181757a4b208514e93.
    
    git revert HEAD
    git push
    git revert HEAD
    git push
    git revert HEAD
    git push

---
+++ src/users/users.controller.ts
@@ -0,0 +1,16 @@
+// src/users/user.controller.ts
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UsersService } from './users.service';
+
+@UseGuards(JwtAuthGuard)
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('me')
+  async getMe(@Req() req: { user: { userId: string; email: string } }) {
+    const userId = req.user.userId; // lấy từ payload trong JWT guard
+    return this.usersService.getMe(userId);
+  }
+}
