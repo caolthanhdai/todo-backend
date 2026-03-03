0e37f54b7e20f471c1c2b779e7b4aa026bd123fe
unknown <thanhdaicoder@gmail.com>
2026-03-03 19:56:06 +0700

    fix file testing

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { TasksService } from './tasks.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { RealtimeModule } from 'src/realtime/realtime.module';
 import { NotificationModule } from 'src/notification/notification.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, RealtimeModule, NotificationModule],
+  imports: [AuthModule, RealtimeModule, NotificationModule, PrismaModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
